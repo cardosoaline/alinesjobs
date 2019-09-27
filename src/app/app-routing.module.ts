cd4f8bdf25c1986eb5209ecea9c3f9569a27c242
Mário <mariosergio952@hotmail.com.br>
2019-09-27 09:39:25 -0300

    add rotas basicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: './pages/auth/auth.module#AuthModule' },
+  { path: 'vagas', loadChildren: './pages/vagas/vagas.module#VagasModule' },
+  { path: 'candidatos', loadChildren: './pages/candidatos/candidatos.module#CandidatosModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
